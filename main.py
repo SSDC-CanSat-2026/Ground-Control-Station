a1f9da19f2d7c92e981d81b5675369eec0444e8f
Admin <admin@Cushman-MacBook-Air.local>
2026-01-23 18:54:55 -0500

    Added a Telemetry Packet class to make it easier to port data around

--- main.py
+++ main.py
@@ -14,6 +14,62 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
+class TelemetryPacket:
+    TEAM_ID = ""
+    MISSION_TIME = ""
+    PACKET_COUNT = ""
+    MODE = ""
+    STATE = ""
+    ALTITUDE = ""
+    TEMPERATURE = ""
+    PRESSURE = ""
+    VOLTAGE = ""
+    CURRENT = ""
+    GYRO_R = ""
+    GYRO_P = ""
+    GYRO_Y = ""
+    ACCEL_R = ""
+    ACCEL_P = ""
+    ACCEL_Y = ""
+    GPS_TIME = ""
+    GPS_ALTITUDE = ""
+    GPS_LATITUDE = ""
+    GPS_LONGITUDE = ""
+    GPS_SATS = ""
+    CMD_ECHO = ""
+
+    def __init__(self, string):
+        fields = string.split(", ")
+        
+        self.TEAM_ID = fields[0]
+        self.MISSION_TIME = fields[1]
+        self.PACKET_COUNT = fields[2]
+        self.MODE = fields[3]
+        self.STATE = fields[4]
+        self.ALTITUDE = fields[5]
+        self.TEMPERATURE = fields[6]
+        self.PRESSURE = fields[7]
+        self.VOLTAGE = fields[8]
+        self.CURRENT = fields[9]
+        self.GYRO_R = fields[10]
+        self.GYRO_P = fields[11]
+        self.GYRO_Y = fields[12]
+        self.ACCEL_R = fields[13]
+        self.ACCEL_P = fields[14]
+        self.ACCEL_Y = fields[15]
+        self.GPS_TIME = fields[16]
+        self.GPS_ALTITUDE = fields[17]
+        self.GPS_LATITUDE = fields[18]
+        self.GPS_LONGITUDE = fields[19]
+        self.GPS_SATS = fields[20]
+        self.CMD_ECHO = fields[21]
+
+    def output_full_str(self):
+
+        return f"{self.TEAM_ID}, {self.MISSION_TIME}, {self.PACKET_COUNT}, {self.MODE}, {self.STATE}, {self.ALTITUDE}, {self.TEMPERATURE}, {self.PRESSURE}, {self.VOLTAGE}, {self.CURRENT}, {self.GYRO_R}, {self.GYRO_P}, {self.GYRO_Y}, {self.ACCEL_R}, {self.ACCEL_P}, {self.ACCEL_Y}, {self.GPS_TIME}, {self.GPS_ALTITUDE}, {self.GPS_LATITUDE}, {self.GPS_LONGITUDE}, {self.GPS_SATS}, {self.CMD_ECHO}"
+    
+
+
 ########################## Global Variables ##########################
 
 # Colors and Fonts for Blue and Orange Theme
@@ -31,8 +87,10 @@ global FONT_TEXT_BOLD;          FONT_TEXT_BOLD = ("Verdana", 14, "bold")
 global FONT_TEXT_BOLD_UNDER;    FONT_TEXT_BOLD_UNDER = ("Verdana", 14, "bold", "underline")
 global FONT_DEBUG;              FONT_DEBUG = ("Verdana", 16, "bold")
 
-# Mission Variables
+# Mission Info Variables
 global TEAM_ID;                 TEAM_ID = "T.B.D."
+
+# Live Variables
 
 ######################################################################
 
